cdb767ff2f8fd6c6cd797c8a7f2579ff530c5bce
dani <dhannizd2@gmail.com>
2025-04-24 14:29:35 +0700

    tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
     <header>
-      <h1>Task Tracker</h1>
+      <h1 class="text-3xl font-bold underline">Task Tracker</h1>
       <nav>
         <router-link to="/tasks">Daftar Tugas</router-link>
       </nav>
